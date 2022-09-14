1d82d7b2e1d92699edfa872ccd28d58881e17897
kishankunvardia <kishankunvardia@hotmail.co.uk>
2022-09-14 12:51:50 +0100

    swagger

--- src/main/java/com/qa/student/StudentServiceSqlApplication.java
+++ src/main/java/com/qa/student/StudentServiceSqlApplication.java
@@ -2,8 +2,10 @@ package com.qa.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class StudentServiceSqlApplication {
 
 	public static void main(String[] args) {
